908cc2913e14d1e2c780f4908ccbd51e47afd357
droneal11015 <droneal11015@gmail.com>
2019-04-09 18:34:09 -0400

    completed project

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@
 const logo = document.querySelector(".main-navigation h1");
 window.onload = function() {logo.classList.add("blink")};
 
-
 //button size scaling up on mouse over
 const btns = document.querySelectorAll(".btn");
 btns.forEach(btn => btn.addEventListener('mouseover', (event) => btn.classList.add("btn-scaling")));
@@ -12,11 +11,11 @@ btns.forEach(btn => btn.addEventListener('mouseover', (event) => btn.classList.a
 //button size scaling back to normal on mouse over
 btns.forEach(btn => btn.addEventListener('mouseout', (event) => btn.classList.remove("btn-scaling")));
 
-//Change first paragrapg to red with double click
+//Change first paragraph to red with single click
 const header_p = document.querySelector(".intro > p");
-header_p.addEventListener('click', (event) => event.target.style.color = 'red');
+header_p.addEventListener('click', (event) => event.target.style.color = '#17A2B8');
 
-//Change back paragraph to black with single click
+//Change back paragraph to black with double click
 header_p.addEventListener('dblclick',(event) => event.target.style.color = 'black');
 
 //keydown alert
@@ -29,3 +28,26 @@ imgs = document.querySelectorAll("img");
 window.addEventListener('scroll', function(event) {
     imgs.forEach(img => img.style.border = "5px solid #17A2B8 "); 
 });
+
+//change background to gold when wheeling mouse
+window.addEventListener('wheel', function(event) {
+    body.style.background = "gold"; 
+});
+
+
+//All anchor tags changed to bold when resizing
+///PREVENT DEFAULT
+const a = document.querySelectorAll("a");
+window.addEventListener('resize', function(event) {
+    a.forEach(anchor => anchor.style.fontWeight = "bold");
+    event.preventDefault();
+});
+
+//will send alert when clicking paragraph
+///NESTED///
+
+header_p.addEventListener('click', (event) => {
+    alert("you have clicked on a paragraph");
+    event.stopPropagation();// stop propagation to the body
+});
+body.addEventListener('click', (event) => alert("you have clicked on the body"));
